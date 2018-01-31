20acb9e55babd2d876ba421263e02ce4c7461e61
WangWeiwei2013 <2621706171@qq.com>
2018-02-01 00:03:34 +0800

    定时任务  每月1月1号生成上月运营数据

--- src/main/java/com/we/controller/quertz/ydate.java
+++ src/main/java/com/we/controller/quertz/ydate.java
@@ -3,11 +3,15 @@ package com.we.controller.quertz;
 import com.we.bean.Ydate;
 import com.we.service.BorrowapplyService;
 import com.we.service.HkbService;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import java.util.Calendar;
 
@@ -15,17 +19,14 @@ import java.util.Calendar;
  * 每月一号生成上个月统计
  * Created by 123456 on 2018/1/5.
  */
-@Controller
-@RequestMapping("/ydate")
-public class ydate {
+@Component
+public class ydate implements Job {
 
     @Autowired
     private HkbService hkbService;
 
-    @RequestMapping("test")
-    @ResponseBody
-    public void execute(){
-
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
         //查询交易总额
         hkbService.AllMoney();
         //当月交易总额
